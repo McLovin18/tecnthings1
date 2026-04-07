23aa353a797db35c7d99525dbd69b9b57d377bb1
McLovin18 <hwcobena@espol.edu.ec>
2026-04-07 14:15:32 -0500

    done

--- tienda-next/app/components/Markdown.tsx
+++ tienda-next/app/components/Markdown.tsx
@@ -5,8 +5,8 @@ export default function Markdown({ children }: { children: string }) {
   return (
     <ReactMarkdown
       components={{
-        strong: ({node, ...props}) => <strong className="font-bold text-purple-700 dark:text-purple-300" {...props} />,
-        b: ({node, ...props}) => <b className="font-bold text-purple-700 dark:text-purple-300" {...props} />,
+        strong: ({node, ...props}) => <strong className="font-bold text-black dark:text-white" {...props} />,
+        b: ({node, ...props}) => <b className="font-bold text-black dark:text-white" {...props} />,
         // Puedes personalizar más etiquetas aquí
       }}
       linkTarget="_blank"
